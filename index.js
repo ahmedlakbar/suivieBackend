c28df04156c4cca1443e71476b9833d21df03dac
unknown <ahmedlakbar@gmail.com>
2023-09-17 19:46:43 +0100

    first commirt

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ app.use(userRouter);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+/*
+const sequelize = require("../config/sequelize");
+const User = require("../models/user.model");
+
+async function syncDB() {
+  try {
+    await sequelize.sync({ force: true });
+    await User.sync({ force: true });
+    console.log("Database and tables synchronized.");
+  } catch (error) {
+    console.error("Error syncing database:", error);
+  }
+}
+
+syncDB();
+
+*/
